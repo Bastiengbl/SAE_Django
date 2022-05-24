8925bf4558bd76a3491591673571c632b1913b9b
etudiant <etudiant@deb11-vdi-info>
2022-05-24 11:51:12 +0200

    commit ajout machine

--- computerApp/urls.py
+++ computerApp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
       path('persos/', views.personnes_list_view, name='persos'),
       path('machine/<pk>', views.machine_detail_view, name='machine-detail'),
       path('personne/<pk>', views.personnes_detail_view, name='persos-detail'),
-
-
+      path('add-machine', views.machine_add_form, name='add-machine'),
+      
 ]
